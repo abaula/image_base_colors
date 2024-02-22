e7a2a284ad627438f723f92067c13d7b5db54e69
A.V.Baula <anton_baula@google.com>
2024-02-22 17:54:05 +0000

    refactoring of code.

--- src/kmeans/histogram_k_means.rs
+++ src/kmeans/histogram_k_means.rs
@@ -16,30 +16,32 @@ pub fn cluster(histogram: &Histogram,
 
     let vec = histogram.get_vec();
 
-    if vec.len() < 1 {
-        return Vec::new();
+    match vec.len() < 1 {
+        true => return Vec::new(),
+        false => (),
     }
 
     let data_size: u32 = vec.len().try_into().expect("Failed convert usize to u32.");
-    let num_dimentions = RgbaColor::get_size();
+    let (data, data_weights) = initialize(&vec);
 
-    let (data, weights) = initialize(&vec);
-
-    let mut clustering = init_clustering(data_size, number_of_clusters);
-    let mut means = allocate_means(number_of_clusters, num_dimentions);
+    let mut data_clusters = init_clustering(data_size, number_of_clusters);
+    let mut cluster_means = allocate_means(number_of_clusters);
     let mut counter: u32 = 0_u32;
 
     while counter < max_try_count {
-        if let Some(new_means) = update_means(&means, &data, &weights, &clustering) {
-            means = new_means;
-        } else {
-            break;
+
+        match update_means(&cluster_means, &data, &data_weights, &data_clusters) {
+            Some(new_means) => {
+                cluster_means = new_means;
+            }
+            None => break,
         }
 
-        if let Some(new_clustering) = update_clustering(&clustering, &data, &means) {
-            clustering = new_clustering;
-        } else {
-            break;
+        match update_clustering(&data_clusters, &data, &cluster_means) {
+            Some(new_clustering) => {
+                data_clusters = new_clustering;
+            }
+            None => break,
         }
 
         counter += 1;
@@ -47,151 +49,152 @@ pub fn cluster(histogram: &Histogram,
 
     let mut centers: Vec<ColorPoint> = Vec::new();
 
-    for i in 0..number_of_clusters {
-        let cluster_count = clustering.iter().filter(|&n| *n == i).count();
+    (0..number_of_clusters).for_each(|i| {
+        let cluster_count = data_clusters.iter().filter(|&n| *n == i).count();
         let cluster_index: usize = i.try_into().expect("Failed convert u32 to usize.");
-        let mean = &means[cluster_index];
+        let mean = &cluster_means[cluster_index];
         let color = RgbaColor::new(mean[0], mean[1], mean[2], mean[3]);
         let weight = cluster_count as f32 / data_size as f32;
         centers.push(ColorPoint::new(color, weight));
-    }
+    });
 
     centers
 }
 
-fn update_means(
-    means: &Vec<Vec<f32>>,
+fn update_means(cluster_means: &Vec<Vec<f32>>,
     data: &Vec<Vec<f32>>,
-    weights: &Vec<f32>,
-    clustering: &Vec<u32>,
-) -> Option<Vec<Vec<f32>>> {
-    let mut new_means = means.clone();
-    let num_clusters = new_means.len();
+    data_weights: &Vec<f32>,
+    data_clusters: &Vec<u32>) -> Option<Vec<Vec<f32>>> {
+
+    let number_of_clusters = cluster_means.len();
 
     // Sum cluster weights.
-    let mut cluster_weights = vec![0_f32; num_clusters];
+    let mut cluster_weights = vec![0_f32; number_of_clusters];
 
-    for (i, weight) in weights.iter().enumerate() {
-        let cluster_number = clustering[i];
+    data_weights.iter().enumerate().for_each(|(i, weight)| {
+        let cluster_number = data_clusters[i];
         let cluster_weight = &mut cluster_weights[cluster_number as usize];
         *cluster_weight += weight;
-    }
+    });
 
-    if cluster_weights.iter().any(|x| is_zero(x)) {
-        return None;
+    match cluster_weights.iter().any(|x| is_zero(x)) {
+        true => return None,
+        false => (),
     }
 
     // Zero means
-    for mean in &mut *new_means {
-        for mean_value in mean {
-            *mean_value = 0f32;
-        }
-    }
+    let mut new_cluster_means = allocate_means(number_of_clusters as u32);
 
     // Accumulate means sum
-    for (i, weight) in weights.iter().enumerate() {
-        let cluster_number = clustering[i];
-        let cluster_means = &mut new_means[cluster_number as usize];
+    data_weights.iter().enumerate().for_each(|(i, weight)| {
+        let cluster_number = data_clusters[i];
+        let cluster_means = &mut new_cluster_means[cluster_number as usize];
         let data_values = &data[i];
 
         for (j, data_value) in data_values.iter().enumerate() {
             cluster_means[j] += data_value * (*weight as f32);
         }
-    }
+    });
 
     // Mean values
-    for (i, cluster_weight) in cluster_weights.iter().enumerate() {
-        let cluster_means = &mut new_means[i];
+    cluster_weights.iter().enumerate().for_each(|(i, cluster_weight)| {
+        let cluster_means = &mut new_cluster_means[i];
 
         for mean_value in cluster_means {
             *mean_value /= *cluster_weight as f32;
         }
-    }
+    });
 
-    Some(new_means)
+    Some(new_cluster_means)
 }
 
-fn update_clustering(
-    clustering: &Vec<u32>,
+fn update_clustering(data_clusters: &Vec<u32>,
     data: &Vec<Vec<f32>>,
-    means: &Vec<Vec<f32>>,
-) -> Option<Vec<u32>> {
-    let num_clusters = means.len();
+    cluster_means: &Vec<Vec<f32>>) -> Option<Vec<u32>> {
+
+    let num_clusters = cluster_means.len();
     let mut changed = false;
     let mut distances = vec![0_f32; num_clusters];
-    let mut new_clustering = clustering.clone();
+    let mut new_data_clusters = data_clusters.clone();
 
-    for (data_ix, data_value) in data.iter().enumerate() {
-        for (cluster_ix, mean) in means.iter().enumerate() {
+    data.iter().enumerate().for_each(|(data_ix, data_value)| {
+        cluster_means.iter().enumerate().for_each(|(cluster_ix, mean)| {
             distances[cluster_ix] = distance(data_value, mean);
+        });
+
+        let new_cluster_ix = min_index(&distances);
+
+        match data_clusters[data_ix] != new_cluster_ix as u32 {
+            true => {
+                changed = true;
+                new_data_clusters[data_ix] = new_cluster_ix as u32;
+            }
+            false => (),
         }
+    });
 
-        let new_cluster_id = min_index(&distances);
-
-        if new_clustering[data_ix] != new_cluster_id as u32 {
-            changed = true;
-            new_clustering[data_ix] = new_cluster_id as u32;
-        }
+    match !changed {
+        true => return None,
+        false => (),
     }
 
-    if !changed {
-        return None;
+    let counts = new_data_clusters.iter().counts();
+
+    match counts.len() < num_clusters {
+        true => return None,
+        false => (),
     }
 
-    let counts = new_clustering.iter().counts();
-
-    if counts.len() < num_clusters {
-        return None;
-    }
-
-    Some(new_clustering)
+    Some(new_data_clusters)
 }
 
 fn distance(point_a: &Vec<f32>, point_b: &Vec<f32>) -> f32 {
     let mut sum_squared_diffs = 0_f32;
 
-    for (i, _) in point_a.iter().enumerate() {
+    point_a.iter().enumerate().for_each(|(i, _)| {
         sum_squared_diffs += (point_a[i] - point_b[i]).powf(2_f32);
-    }
+    });
 
     sum_squared_diffs.sqrt()
 }
 
 fn min_index(values: &Vec<f32>) -> usize {
+
     let mut index_of_min_value: usize = 0;
     let mut small_value = values[index_of_min_value];
 
-    for (i, value) in values.iter().enumerate() {
+    values.iter().enumerate().for_each(|(i, value)| {
         if *value < small_value {
             small_value = *value;
             index_of_min_value = i;
         }
-    }
+    });
 
     index_of_min_value
 }
 
 fn initialize(histogram: &Vec<ColorPoint>) -> (Vec<Vec<f32>>, Vec<f32>) {
+
     let mut data: Vec<Vec<f32>> = Vec::new();
     let mut weights: Vec<f32> = Vec::new();
 
-    for entry in histogram {
+    histogram.iter().for_each(|entry| {
         data.push(entry.color.to_vec());
         weights.push(entry.weight);
-    }
+    });
 
     (data, weights)
 }
 
 fn init_clustering(size: u32, number_of_clusters: u32) -> Vec<u32> {
+
     let mut rng = rand::thread_rng();
 
     let clustering: Vec<u32> = (0..size)
         .map(|x| {
-            if x < number_of_clusters {
-                x
-            } else {
-                rng.gen_range(0..number_of_clusters)
+            match x < number_of_clusters {
+                true => x,
+                false => rng.gen_range(0..number_of_clusters),
             }
         })
         .collect();
@@ -199,7 +202,8 @@ fn init_clustering(size: u32, number_of_clusters: u32) -> Vec<u32> {
     clustering
 }
 
-fn allocate_means(num_clusters: u32, num_dimentions: u32) -> Vec<Vec<f32>> {
+fn allocate_means(num_clusters: u32) -> Vec<Vec<f32>> {
+    let num_dimentions = RgbaColor::get_size();
     vec![vec![0_f32; num_dimentions as usize]; num_clusters as usize]
 }
 
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use crate::{
 fn main() {
     let img_path = "/home/dev/work/image_base_colors/images/800px-Cat_November_2010-1a.jpg";
     let histogram = histogram::from_image_path(img_path);
-    let mut centers = histogram_k_means::cluster(&histogram, 4, 30);
+    let mut centers = histogram_k_means::cluster(&histogram, 4, 10);
 
     centers.sort_by(|a, b| b.weight.total_cmp(&a.weight));
 
